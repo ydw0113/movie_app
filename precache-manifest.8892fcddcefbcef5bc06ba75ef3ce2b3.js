b5e203a860cabe8ea9e4309d9c5ec736462392e2
ydw0113 <ydw0113@users.noreply.github.com>
2019-03-19 17:57:06 +0900

    Updates

---
+++ precache-manifest.8892fcddcefbcef5bc06ba75ef3ce2b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "1a0c0721022e566a48c4",
+    "url": "/movie_app/static/js/main.1a0c0721.chunk.js"
+  },
+  {
+    "revision": "1bf244e142401c5b4106",
+    "url": "/movie_app/static/js/1.1bf244e1.chunk.js"
+  },
+  {
+    "revision": "1a0c0721022e566a48c4",
+    "url": "/movie_app/static/css/main.819664dc.chunk.css"
+  },
+  {
+    "revision": "70b4fda62df92c82ba93374951337d6e",
+    "url": "/movie_app/index.html"
+  }
+];
